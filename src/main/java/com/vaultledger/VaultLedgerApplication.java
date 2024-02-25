8aef0438a249d557ee4418273aad56c24021e1f6
Jsingh651 <jangsing02@gmail.com>
2024-02-25 19:39:38 +0000

    build transaction rest controller (part 2)

--- src/main/java/com/vaultledger/VaultLedgerApplication.java
+++ src/main/java/com/vaultledger/VaultLedgerApplication.java
@@ -6,3 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VaultLedgerApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(VaultLedgerApplication.class, args);
+    }
+}
+
+
+
+
