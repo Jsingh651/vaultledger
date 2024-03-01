3cd34ac95448d812ef19ca751f38e3ba42975963
Jsingh651 <jangsing02@gmail.com>
2024-03-01 17:15:58 +0000

    add gitignore for maven target

---
+++ src/main/java/com/vaultledger/config/WebConfig.java
@@ -0,0 +1,11 @@
+package com.vaultledger.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
