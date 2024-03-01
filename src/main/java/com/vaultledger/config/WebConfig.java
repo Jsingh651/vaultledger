afddb12d4fb7d0675d6fb2df272812ce3f32d7db
Jsingh651 <jangsing02@gmail.com>
2024-03-01 18:55:36 +0000

    add transaction type enum (part 2)

--- src/main/java/com/vaultledger/config/WebConfig.java
+++ src/main/java/com/vaultledger/config/WebConfig.java
@@ -9,3 +9,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
+}
+
