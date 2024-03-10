0e0cde9b8021da6ffcc40624ee832eb4618e00b6
Jsingh651 <jangsing02@gmail.com>
2024-03-10 18:38:34 +0000

    improve budget percent calculation

---
+++ src/main/java/com/vaultledger/controller/DashboardController.java
@@ -0,0 +1,10 @@
+package com.vaultledger.controller;
+
+import com.vaultledger.dto.DashboardSummary;
+import com.vaultledger.service.FinanceService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
