00c28e8e6aa0c039c9404b31265513de9f558248
Jsingh651 <jangsing02@gmail.com>
2024-05-14 20:44:57 +0000

    build dashboard html page

---
+++ src/main/java/com/vaultledger/dto/CategoryBudgetStatus.java
@@ -0,0 +1,10 @@
+package com.vaultledger.dto;
+
+public class CategoryBudgetStatus {
+
+    private String category;
+    private double spent;
+    private double limit;
+    private double percentUsed;
+
+    public CategoryBudgetStatus() {}
