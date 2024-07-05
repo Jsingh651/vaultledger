15f211971769d3c4110342cea34a525f8ae8b05f
Jsingh651 <jangsing02@gmail.com>
2024-07-05 13:42:52 +0000

    minor cleanup (part 2)

--- src/main/java/com/vaultledger/dto/DashboardSummary.java
+++ src/main/java/com/vaultledger/dto/DashboardSummary.java
@@ -10,3 +10,15 @@ public class DashboardSummary {
     private double balance;
     private Map<String, Double> expensesByCategory;
     private List<CategoryBudgetStatus> budgetStatus;
+
+    public DashboardSummary() {}
+
+    public double getTotalIncome() { return totalIncome; }
+    public void setTotalIncome(double totalIncome) { this.totalIncome = totalIncome; }
+
+    public double getTotalExpenses() { return totalExpenses; }
+    public void setTotalExpenses(double totalExpenses) { this.totalExpenses = totalExpenses; }
+
+    public double getBalance() { return balance; }
+    public void setBalance(double balance) { this.balance = balance; }
+
