e7f309f2b7f309f6f88f535de9b0cd6d5b045bae
Jsingh651 <jangsing02@gmail.com>
2024-08-10 22:51:39 +0000

    fix typo in comment (part 4)

--- src/main/java/com/vaultledger/model/Transaction.java
+++ src/main/java/com/vaultledger/model/Transaction.java
@@ -40,3 +40,17 @@ public class Transaction {
     public void setDescription(String description) { this.description = description; }
 
     public Double getAmount() { return amount; }
+    public void setAmount(Double amount) { this.amount = amount; }
+
+    public TransactionType getType() { return type; }
+    public void setType(TransactionType type) { this.type = type; }
+
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+
+    public LocalDate getDate() { return date; }
+    public void setDate(LocalDate date) { this.date = date; }
+}
+
+
+
