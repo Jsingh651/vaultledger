aeea042c0aec88ebbd101ccea175614f46ce4496
Jsingh651 <jangsing02@gmail.com>
2024-08-10 22:32:32 +0000

    fix lint warning (part 3)

--- src/main/java/com/vaultledger/model/Transaction.java
+++ src/main/java/com/vaultledger/model/Transaction.java
@@ -26,3 +26,17 @@ public class Transaction {
     private TransactionType type;
 
     @NotBlank
+    private String category;
+
+    @NotNull
+    private LocalDate date;
+
+    public Transaction() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public Double getAmount() { return amount; }
