f608638834fa1c09b32e042974622687fa64706a
Jsingh651 <jangsing02@gmail.com>
2024-09-24 13:24:07 +0000

    minor performance tweak

---
+++ src/main/java/com/vaultledger/repository/BudgetRepository.java
@@ -0,0 +1,10 @@
+package com.vaultledger.repository;
+
+import com.vaultledger.model.Budget;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
